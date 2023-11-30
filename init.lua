f9080923399cea85c8ff45d011aa56b16540bed3
CatMan <garfield_lv@163.com>
2023-11-30 17:12:21 +0800

    start

--- init.lua
+++ init.lua
@@ -1,10 +1,2 @@
--- load base config
-require("catman.core.options")
-require("catman.core.keymaps")
---setup lazy
-require("catman.plugins_setup_lazy")
-require("catman.core.colorscheme")
-require("catman.core.vimplug")
-
--- load extension config
--- vim.cmd("colorscheme tokyonight-moon")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
