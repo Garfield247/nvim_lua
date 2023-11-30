80bb33afd17fa249046383d5c38c5740d13eeafe
CatMan <garfield_lv@163.com>
2023-11-30 18:05:59 +0800

    base option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,69 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local opt = vim.opt
+
+-- 显示行号/和相对行号
+opt.number = true
+opt.relativenumber = true
+-- opt.ambiwidth = double
+
+-- tabs & indentation
+opt.tabstop = 4 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 4 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.smarttab = true
+opt.smartindent = true -- copy indent from current line when starting new one
+
+opt.swapfile = false
+-- line wrapping
+opt.wrap = true -- disable line wrapping
+
+-- search settings
+opt.incsearch = true
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
+
+-- appearance
+
+-- turn on termguicolors for nightfly colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
+opt.termguicolors = true
+-- opt.background = "light" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+opt.iskeyword:append("-") -- consider string-string as whole word--vi
+
+opt.showcmd = true
+opt.wildmenu = true
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+--记录上次浏览位置
+--保存时删除行尾空白
+vim.cmd([[
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+auto BufWritePre * sil %s/\s\+$//ge "
+function! ViewInBrowser()
+    let file = expand("%:p")
+    if has("mac")
+        let browsers = "open -a \"Google Chrome\""
+        exec ":silent ! ". browsers  file
+    endif
+endfunction
+]])
